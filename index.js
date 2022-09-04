83e30f357880d1b4a16f4fa84fe4679159206915
SiobhanMcCaffrey <siobhanamccaffrey@gmail.com>
2022-09-04 09:05:24 -0400

    commit tree b33d36739060ba18c30a5b53d833712587b90094

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector ('#main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id","victory")
+
+newHeader.innerHTML = "Siobhan is the champion."
+document.body.append(newHeader)
